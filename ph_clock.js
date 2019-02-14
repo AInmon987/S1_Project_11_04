904eb77f4b1c51d701b48e971e5c4030c61ce810
austin inmon <Ainmon987@west-mec.org>
2019-02-14 15:22:49 -0700

    Added countdown clock/ Final commit
    Added The variables set the value to 30 min and 0 seconds. Added the variable that makes The time left on the clock will by multiply by 60 and added to the seconds left. The clock will count down by 1000 milliseconds. The leading zero will make sure that the leading digit will be a 0 unless sec/minutes is above ten . Changes the minutes/seconds string to the proper value. Added var where When the clock hits zero and if form is not done, the message "Order Expired" will show. var where clock from rerunning when clock hits zero.  Ready for review.

--- ph_clock.js
+++ ph_clock.js
@@ -24,7 +24,40 @@
    ticket order. If the time left is 0, the stopClock() function is run;
    otherwise nothing happens and the program continues to run. */
 
-
+// The varible set the value to 30 min. 
+var minsLeft = 30;
+//  The var sets the value to 0 seconds
+var secsLeft = 0;
+//  The time left on the clock will by multiply by 60 and added to the seconds left.
+var timeLeft = (minsLeft * 60 + secsLeft);
+//  The clock will count down by 1000 milliseconds.
+var clockID = setInterval("countdown()", 1000);
+// The function has a name of countdown. 
+function countdown() {
+    // The time will be divided and rounded to nears lowest digit.
+    minsLeft = Math.floor(timeLeft / 60);
+    //
+    secsLeft = timeLeft - 60 * minsLeft;
+    //  The leading zero will make sure that the leading digit will be a 0 unless min is above ten .
+    var minsString = addLeadingZero(minsLeft);
+    //  The leading zero will make sure that the leading digit will be a 0 unless sec is above ten .
+    var secsString = addLeadingZero(secsLeft);
+    //  Changes the minutes string to the proper value.
+    document.getElementById('minutes').innerHTML = minsString;
+    //  Changes the seconds string to the proper value.
+    document.getElementById('seconds').innerHTML = secsString;
+    //  Added the function of checkTimer. 
+    checkTimer();
+    // Counts down by one.
+    timeLeft--;
+}
+// Function name stopClock. 
+function stopClock() {
+    // When the clokc hits zero and if form is not done, the message "Order Expired" will show.
+    document.getElementById('TimeHead').insertAdjacentHTML("beforeend", "<br />(Order Expired)");
+    // Keeps the clock from rerunning when clock hits zero. 
+    clearInterval(clockID);
+}
 
 function checkTimer() {
     if (timeLeft === 0) stopClock();
