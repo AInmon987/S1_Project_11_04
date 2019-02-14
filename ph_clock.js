95fe9ca75b75a76950eaec93123f0506d6f69b7d
austin inmon <Ainmon987@west-mec.org>
2019-02-14 15:05:01 -0700

    initial commit
    Changed the javascript and html file names to proper formation.  Added the script link for the javascript file. Added use strict on javascript file.

--- ph_clock.js
+++ ph_clock.js
@@ -1,11 +1,12 @@
+"use strict";
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Case Problem 4
 
    Countdown Clock for Ticket Ordering
-   Author: 
-   Date:  
+   Author: Austin Inmon
+   Date:  2.14.19
 
    Filename:   ph_clock.js     
 
@@ -22,15 +23,17 @@
 /* The checkTimer() function tests whether there is any time left to make the
    ticket order. If the time left is 0, the stopClock() function is run;
    otherwise nothing happens and the program continues to run. */
-   
+
+
+
 function checkTimer() {
-   if (timeLeft === 0) stopClock();
+    if (timeLeft === 0) stopClock();
 }
 
 
 /* The addLeadingZero() function adds a leading zero to values which are less than 10 */
 
 function addLeadingZero(num) {
-   var numStr = (num < 10) ? ("0" + num) : "" + num;
-   return numStr;
+    var numStr = (num < 10) ? ("0" + num) : "" + num;
+    return numStr;
 }
